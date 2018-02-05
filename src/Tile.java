4bf7044ab56afd958e1fd4bac8483b6af7977acd
Mikael John Andersson <mickepunktjohn@gmail.com>
2018-02-05 16:50:45 +0100

    Changes to tile

--- src/Tile.java
+++ src/Tile.java
@@ -3,19 +3,10 @@ import javax.swing.plaf.metal.MetalButtonUI;
 
 public class Tile extends JButton {
 
-    int row, col;
 
     //Setting appereance of button
     public Tile() {
-        row = 0;
-        col = 0;
         setOpaque(true);
         setUI(new MetalButtonUI());
-    }
-
-    public Tile(int row, int col) {
-        super();
-        this.row = row;
-        this.col = col;
     }
 }
