41643109548ef21a5f9dd1b4261302a20addb228
Mikael John Andersson <mickepunktjohn@gmail.com>
2018-02-19 12:43:03 +0100

    Fixade streams och hashmap

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-//TODO: Lägga in värden på pieces
+
 //TODO: Fixa inkapsling - getters/setters
 //TODO: Fixa lambdas och streams
 //TODO: Fixa game-over mekanik
 //TODO: Fixa så att players gör drag när man trycker på tangent
-//TODO: Fixa så att bildikonerna inte hör till piece-subklasserna
 //TODO: Snygga till utskrifterna med formatterad text
 //TODO: Ta bort dubblering av kod
 //TODO: Lägga in mer pieces
-
 
 
 public class Main {
