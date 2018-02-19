7d502545e1f9b7443bae54b2898daad855d43d6c
Mikael John Andersson <mickepunktjohn@gmail.com>
2018-02-19 16:20:06 +0100

    La till king

---
+++ src/King.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class King extends Piece{
+
+    public King(ChessColor chessColor, ArrayList<Piece> list, Tile tile) {
+        super(chessColor, list, tile);
+        this.value = 100;
+    }
+
+    @Override
+    public boolean isMovable(Move move) {
+
+        if(!move.toTile.hasPiece() || move.toTile.hasEnemyPiece(this.chessColor)){
+        return move.isOneStepAnyInAnyDirection();
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return this.chessColor + " KING";
+    }
+}
